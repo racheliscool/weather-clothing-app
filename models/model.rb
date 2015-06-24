ba1d2e812f9a8a4ceafe7094b29c9e78f42c9a74
izchandler22 <isskisoc8@gmail.com>
2015-06-24 18:33:49 +0000

    yay

--- models/model.rb
+++ models/model.rb
@@ -1,8 +1,20 @@
+require 'open-uri'
+
+GET http://api.wunderground.com/api/4966e73960475833/features/settings/q/query.format
+
 class Weather
   attr_accessor :zip_code
   def initialize(zip_code)
     @zip_code = zip_code
   end
+  
+  
+  
+
+
+end
+  
+ 
   
   
   
