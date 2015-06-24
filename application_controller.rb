25b1799612a63f8100b1fefdba410d18594abfe5
izchandler22 <isskisoc8@gmail.com>
2015-06-24 18:14:08 +0000

    more app.rb stuff

--- application_controller.rb
+++ application_controller.rb
@@ -6,5 +6,18 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  post '/weather' do 
+    @zip_code = params["user_input"]
+    
+    zip_code = Weather.new(zip_code)
+    @translated_string = translator.translate
+    erb :results
+  end
+  
+  get "/songs" do
+    @song = ["uptown funk", "let it go", "stay close"]
+    erb :songs
+  end
 
 end
--- models/model.rb
+++ models/model.rb
@@ -1,4 +1,8 @@
 class Weather
+  attr_accessor :zip_code
+  def initialize(zip_code)
+    @zip_code = zip_code
+  end
   
   
   
