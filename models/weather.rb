7edd2b4d60769a8dd959ea189d06ed998aa1cb73
izchandler22 <isskisoc8@gmail.com>
2015-06-25 17:17:27 +0000

    yeet

--- models/weather.rb
+++ models/weather.rb
@@ -3,7 +3,7 @@ require 'json'
 
 # GET http://api.wunderground.com/api/4966e73960475833/features/settings/q/query.format
 class Weather
-  attr_reader :state, :city
+  attr_reader :state, :city, :temp_f, :description
   def initialize(state, city)
     @state = state.upcase
     @city = city.gsub(" ", "%20")
@@ -15,13 +15,23 @@ class Weather
     
     #url = "http://api.wunderground.com/api/4966e73960475833/forecast/geolookup/conditions/q/#{state}/#{city}.json"
     opened_page = open(url)
-    forcast_data = JSON.load(opened_page)
-    puts forcast_data.inspect
+    @forcast_data = JSON.load(opened_page)
+    puts @forcast_data.inspect
   end
   
   def where_are_you
     # code here    
   end
+  
+  def fetch_temperature
+    @temp_f=@forcast_data["current_observation"]["temp_f"]
+  end
+  
+  def fetch_description
+    @description=@forcast_data["forecast"]["txt_forecast"]["forecastday"][0]["icon"]
+  end
+  
+  
   
   
 end
